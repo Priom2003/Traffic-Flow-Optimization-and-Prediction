7180d84558097231c37e5e9161d53032c85ad4a2
Priom2003 <duttapriom79@gmail.com>
2025-11-27 15:27:31 +0530

    Delete Extra directory

--- Extra/drop.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("fetch_tweets.csv")
-
-df = df.drop_duplicates(subset=["raw_text_tweet"], keep="first").reset_index(drop=True)
-print(f"Shape after removing duplicates: {df.shape}")
-df.to_csv("fetch_cleaned.csv", index=False)
